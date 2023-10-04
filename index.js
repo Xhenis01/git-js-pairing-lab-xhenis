9cb4a180b9117710f4cc63d3ce0e45368e37cb10
xhenis <xhenism2001@gmail.com>
2023-10-04 14:29:47 -0400

    5 to 100 count check it out Jordy

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Code your solutions in this file
-const add = (num1,num2) => {
+/*const add = (num1,num2) => {
     return(num1+num2)
 }
 console.log(add(20,22))
@@ -22,3 +22,12 @@ const division = (num1,num2) => {
     return num1 / num2;
 }
 console.log(division(20,22))
+test
+*/// test //
+
+const fiveToOneHundred = () => {
+    for(let i = 5; i <= 100; i++){
+        console.log(i)
+    }
+} 
+fiveToOneHundred()
