7315c108e248db948b5ea8f03a8e507b09e68d70
xhenis <xhenism2001@gmail.com>
2023-10-05 15:45:07 -0400

    Waiting for check off everything completed. Good work team.

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
 //Code your solutions in this file
 
-
 //1
 const fiveToOneHundred = () => {
     for(let i = 5; i <= 100; i++){
         console.log(i);
     }
 } 
-    fiveToOneHundred();
+    fiveToOneHundred(); 
+
 //2
 const multiplesOfThree = ()=>{
     for(let i = 3; i <= 100; i += 3){
         console.log(i);
     }
 }
-    multiplesOfThree();
+    multiplesOfThree(); 
+
+//3 
+ 
+    const multiplesOfThreeOrFive = () => {
+        for(let i= 3; i <= 100; i ++){
+            if(i% 3 === 0 || i% 5 === 0){
+                console.log(i);
+        }
+    }
+    }
+    multiplesOfThreeOrFive();
 
 //4
 const untilNum = (num2)=> {
     for(let i = 1; i <= num2; i ++){
-        console.log(i)
+        console.log(i);
     }
 }
-    untilNum(9);
+    untilNum(5); 
+    untilNum(9); 
+    untilNum(42); 
 
+//5
+const multiply =(num1,num2) => {
+    return(num1 * num2);
+}
+console.log(multiply(2, 4));
+console.log(multiply(10, -5));
+console.log(multiply(3, 7.5));
 
 //6 
 const add = (num1,num2)=> {
@@ -33,8 +53,23 @@ const add = (num1,num2)=> {
         return num1 + num2;
     }
 }
+   console.log(add(2,4));
+   console.log(add(10,-5));
+   console.log(add(3,7.5));
+   console.log(add(5,5));
    console.log(add(6,6));
 
+//7 
+const isNegative = (num1) => {
+    if(num1 >= 0){
+        return false;
+    }else {
+        return true;
+    }
+}
+console.log(isNegative(3));
+console.log(isNegative(-2));
+console.log(isNegative(Math.PI));
 
 //8
 const triangleArea = (base, height)=> {
@@ -42,7 +77,23 @@ const triangleArea = (base, height)=> {
    return areaOfTriangle;
 
 }
+    console.log(triangleArea(5,7));
     console.log(triangleArea(6,8));
+
+//9  
+const betweenTwentyAndFourty = (num1) => {
+    if(num1 > 20 && num1 < 40){
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(betweenTwentyAndFourty(4));
+console.log(betweenTwentyAndFourty(21));
+console.log(betweenTwentyAndFourty(99));
+/*line 86 return false but on README.md 
+says ture README.md is wrong*/
+console.log(betweenTwentyAndFourty(101));
 
 //10
 const largest = (num1,num2,num3)=> {
@@ -54,11 +105,6 @@ const largest = (num1,num2,num3)=> {
         return num3;
     }
 }
-   console.log(largest(41,108,86));
-    const multiplesOfThreeOrFive = () => {
-        for(let i= 3; i <= 100; i ++){
-            console.log(i)
-        }
-    }
-
-    multiplesOfThreeOrFive()
+console.log(largest(4,6,8));
+console.log(largest(30,22,17));
+console.log(largest(41,108,86));
